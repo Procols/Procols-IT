d6a555baa49f94ec4c242c09f2ca24f51e8e3d34
Boopesh1130 <boopesh.3083@gmail.com>
2025-06-21 21:07:21 +0530

    Completed Images Only Pending

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('seo', views.seo, name='seo'),
+    path('service', views.service, name='service'),
+    path('webapp', views.webapp, name='webapp'),
+    path('digitalmarketing', views.digitalmarketing, name='digitalmarketing'),
+    path('aboutus', views.aboutus, name='aboutus'),
+    path('contactus', views.contactus, name='contactus')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def seo(request):
+    return render(request, 'seo.html')
+
+def webapp(request):
+    return render(request, 'webapp.html')
+
+def digitalmarketing(request):
+    return render(request, 'digitalmarketing.html')
+
+def aboutus(request):
+    return render(request, 'aboutus.html')
+
+def contactus(request):
+    return render(request, 'contactus.html')
